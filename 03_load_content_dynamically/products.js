b50ba8afcb785394f89df47e97b4865b14b99790
Marcos de la Calle <marcos.dlcs@gmail.com>
2023-07-17 18:52:37 +0200

    Section three. External module with product info. Basic JS files

---
+++ 03_load_content_dynamically/products.js
@@ -0,0 +1,82 @@
+export const products = [
+  {
+    title: "Cute family house",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem neque molestias veniam velit dolores odit hic, temporibus cum aperiam, nam voluptates quaerat vel, distinctio quae nobis omnis consectetur repudiandae unde",
+    picture: "h01.webp",
+    priceInEuros: 199021,
+    tags: ["family"],
+  },
+  {
+    title: "French palace",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem neque molestias veniam velit dolores odit hic, temporibus cum aperiam, nam voluptates quaerat vel, distinctio quae nobis omnis consectetur repudiandae unde",
+    picture: "h02.webp",
+    priceInEuros: 2425000,
+    tags: ["distinguished", "royal-families"],
+  },
+  {
+    title: "The red farm",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem neque molestias veniam velit dolores odit hic, temporibus cum aperiam, nam voluptates quaerat vel, distinctio quae nobis omnis consectetur repudiandae unde",
+    picture: "h03.webp",
+    priceInEuros: 1879000,
+    tags: ["animals"],
+  },
+  {
+    title: "A piece of the paradise",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem neque molestias veniam velit dolores odit hic, temporibus cum aperiam, nam voluptates quaerat vel, distinctio quae nobis omnis consectetur repudiandae unde",
+    picture: "h04.webp",
+    priceInEuros: 443000,
+    tags: ["nature"],
+  },
+  {
+    title: "US family sitcom house",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem neque molestias veniam velit dolores odit hic, temporibus cum aperiam, nam voluptates quaerat vel, distinctio quae nobis omnis consectetur repudiandae unde",
+    picture: "h05.webp",
+    priceInEuros: 32000,
+    tags: ["family"],
+  },
+  {
+    title: "Modern style",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem neque molestias veniam velit dolores odit hic, temporibus cum aperiam, nam voluptates quaerat vel, distinctio quae nobis omnis consectetur repudiandae unde",
+    picture: "h06.webp",
+    priceInEuros: 567000,
+    tags: ["modern"],
+  },
+  {
+    title: "The scary house in the forest",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem neque molestias veniam velit dolores odit hic, temporibus cum aperiam, nam voluptates quaerat vel, distinctio quae nobis omnis consectetur repudiandae unde",
+    picture: "h07.webp",
+    priceInEuros: 199000,
+    tags: ["nature", "murder"],
+  },
+  {
+    title: "Miami vice",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem neque molestias veniam velit dolores odit hic, temporibus cum aperiam, nam voluptates quaerat vel, distinctio quae nobis omnis consectetur repudiandae unde",
+    picture: "h08.webp",
+    priceInEuros: 1328000,
+    tags: ["modern"],
+  },
+  {
+    title: "Cool green roof",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem neque molestias veniam velit dolores odit hic, temporibus cum aperiam, nam voluptates quaerat vel, distinctio quae nobis omnis consectetur repudiandae unde",
+    picture: "h09.webp",
+    priceInEuros: 349000,
+    tags: ["nature", "loneliness"],
+  },
+  {
+    title: "Pirates of the Caribbean",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem neque molestias veniam velit dolores odit hic, temporibus cum aperiam, nam voluptates quaerat vel, distinctio quae nobis omnis consectetur repudiandae unde",
+    picture: "h10.webp",
+    priceInEuros: 234000,
+    tags: ["nature", "adventure"],
+  },
+];
